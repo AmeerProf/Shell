9f0d2f91ba9052759dbc05714711f1c8307578fe
AmeerProf <skameerprof@gmail.com>
2025-09-09 14:04:05 +0530

    Force commit

--- Disk_Space.sh
+++ Disk_Space.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DISK_SPACE=$(df -hT | grep "ext4")
+DISK_USAGE=$(df -hT | grep "ext4")
+MESSAGE=""
 G="\e[32m"
 N="\e[0m"
 
@@ -9,7 +10,12 @@ echo -e  " $G $DISK_SPACE $N"
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" f1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+    fi
+done <<< $DISK_USAGE
 
-done <<< $DISK_SPACE
+echo -e "Message: $MESSAGE"
